30dd3337c1f204e0f845723040d53a97ada152d6
Tanmay Sharma <sharmatanmay617@gmail.com>
2022-06-24 15:47:31 +0530

    Add typescript interfaces for incoming json.

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ The Link to the deployed project can be found here: https://tanmaysharma2001.git
 History:
  - Deployed the website using HTML & CSS
  - Used javascript to fetch weather data of the Innopolis City and flexbox to align "Skills" Section.
- - Used TypeScript and Rollup Bundler in the project.
+ - Used TypeScript Interfaces for incoming JSON and Rollup Bundler in the project.
+ - Used Svelte Framework for reactivity.
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,42 @@ export { };
 
 var urlOfAPI = "https://weatherdbi.herokuapp.com/data/weather/innopolis";
 
+interface jsonData {
+    contact_author: Map<string, string>;
+    currentConditions: currentConditions;
+    data_source: string;
+    next_days: nextDaysElement[];
+    region: string;
+}
+
+interface currentConditions {
+    comment: string;
+    dayhour: string;
+    humidity: string;
+    iconURL: string;
+    precip: string;
+    temp: genericTemp;
+    wind: genericWind;
+}
+
+interface nextDaysElement {
+    comment: string;
+    day: string;
+    iconURL: string;
+    max_temp: genericTemp;
+    min_temp: genericTemp;
+}
+
+interface genericTemp {
+    c: number;
+    f: number;
+}
+
+interface genericWind {
+    km: number;
+    mile: number;
+}
+
 const CommentElement: HTMLSpanElement = document.querySelector("#comment") as HTMLSpanElement;
 const DayHourElement: HTMLSpanElement = document.querySelector("#dayhour") as HTMLSpanElement;
 const HumidityElement: HTMLSpanElement = document.querySelector("#humidity") as HTMLSpanElement;
@@ -12,17 +48,19 @@ function getData(): void {
     fetch(urlOfAPI).then((response) => {
         return response.json()
     }).then((data => {
+
+        const newData: jsonData = data;
         
         // !----- Important -----!
         // Note sometimes this API crashes for unknow reasons. So please try later,
         // or please contact me if it doesn't work.
         // This code works fine.
         // Thank you...!
-        CommentElement.textContent = data.currentConditions.comment;
-        DayHourElement.textContent = data.currentConditions.dayhour;
-        HumidityElement.textContent = data.currentConditions.humidity;
-        TempElement.textContent = data.currentConditions.temp.c;
-        WindElement.textContent = data.currentConditions.wind.km;
+        CommentElement.textContent = newData.currentConditions.comment;
+        DayHourElement.textContent = newData.currentConditions.dayhour;
+        HumidityElement.textContent = newData.currentConditions.humidity;
+        TempElement.textContent = (newData.currentConditions.temp.c).toString();
+        WindElement.textContent = (newData.currentConditions.wind.km).toString();
     }))
 }
 
