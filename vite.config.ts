00481a3be3851ef4cc6e65ecd8ae462320cacee9
Tanmay Sharma <sharmatanmay617@gmail.com>
2024-05-06 04:37:25 +0300

    Path Modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   resolve: {
     alias: {
-      '@': require('path').resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src')
     }
   },
 })
