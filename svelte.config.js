7720be0c7fa8b4382b9a362e71e80a5aac7dd0c7
Tanmay Sharma <sharmatanmay617@gmail.com>
2024-05-06 04:13:47 +0300

    Svelte Config Edited

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/tanmaysharma2001.github.io' : '',
+  }
+  },
+};
